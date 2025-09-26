2db581d023370c5a7c3055f14121a4e65ebff5cc
Aniket <aniketmaneedu@gmail.com>
2025-09-26 11:04:01 +0100

    Spring boot project created for developing APIs.

---
+++ apis/src/main/java/com/aicoder/apis/AicoderApplication.java
@@ -0,0 +1,13 @@
+package com.aicoder.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicoderApplication.class, args);
+	}
+
+}
